1cdbf2c914ab00d27fc5f8ee0309a503be135858
Borja Soler <borjasolrey@gmail.com>
2023-10-16 18:01:47 +0200

    added all the links to the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,21 +7,38 @@ const Footer = () => (
   >
     <div className="flex space-x-4 p-2 z-60">
       <a href="mailto:team@hello.app" target="_blank" rel="noopener noreferrer">
-        <i className="zIndex-100 fas fa-envelope text-gray-300 hover:text-gray-500" />
+        <i className="h-5 w-5 zIndex-100 fas fa-envelope text-gray-300 hover:text-gray-500" />
       </a>
       <a
         href="https://www.linkedin.com/company/hellostorage"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i className="fab fa-linkedin text-gray-300 hover:text-gray-500" />
+        <i className="h-5 w-5 fab fa-linkedin text-gray-300 hover:text-gray-500" />
       </a>
       <a
         href="https://github.com/hello-storage"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i className="fab fa-github text-gray-300 hover:text-gray-500" />
+        <i className="h-5 w-5 fab fa-github text-gray-300 hover:text-gray-500" />
+      </a>
+      <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+        <i className="h-5 w-5 fab fa-twitter text-gray-300 hover:text-gray-500" />
+      </a>
+      <a
+        href="https://www.instagram.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="h-5 w-5 fab fa-instagram text-gray-300 hover:text-gray-500" />
+      </a>
+      <a
+        href="https://www.tiktok.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="h-5 w-5 fab fa-tiktok text-gray-300 hover:text-gray-500" />
       </a>
     </div>
     © 2023 hello.app | all rights reserved.
