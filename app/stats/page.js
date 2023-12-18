e44e30dadd1693ee0740fdfb431a28d0dd5ea729
Onetti01 <113169413+Onetti01@users.noreply.github.com>
2023-12-18 10:29:03 +0100

    fix-error-vercel

--- app/stats/page.js
+++ app/stats/page.js
@@ -1,5 +1,5 @@
 import { Footer, Navbar } from "../../components";
-import Statistics from "../../components/Statistics";
+
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden h-screen text-gray-100">
