a07cc6442b7d962532540999933f136a8de9417a
Onetti01 <113169413+Onetti01@users.noreply.github.com>
2023-12-18 10:23:32 +0100

    fix-problems & responsive

--- app/page.js
+++ app/page.js
@@ -1,12 +1,47 @@
+"use client";
+
+import React, { useEffect } from "react";
 import { Footer, Navbar } from "../components";
 import { Hero } from "../sections";
+import ParticleAnimation from "../utils/particles";
 
-const Page = () => (
-  <div className="bg-primary-black overflow-hidden h-screen text-gray-100">
-    <Navbar />
-    <Hero />
-    <Footer />
-  </div>
-);
+const Page = () => {
+  useEffect(() => {
+    const canvasElements = document.querySelectorAll(
+      "[data-particle-animation]"
+    );
+    canvasElements.forEach((canvas) => {
+      const options = {
+        quantity: canvas.dataset.particleQuantity,
+        staticity: canvas.dataset.particleStaticity,
+        ease: canvas.dataset.particleEase,
+      };
+      new ParticleAnimation(canvas, options);
+    });
+  }, []);
+
+  return (
+    <div className="relative overflow-hidden h-screen text-gray-100 bg-slate-950">
+      <div className="absolute inset-0 bg-[url('https://media.publit.io/file/-Pngtree-noise-texture-black-background-1575880.png')] opacity-5"></div>
+      <canvas
+        data-particle-animation
+        data-particle-quantity="100"
+        data-particle-staticity="50"
+        data-particle-ease="50"
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          zIndex: 0,
+        }}
+      />
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
+};
 
 export default Page;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,11 +9,25 @@ const Navbar = () => (
     variants={navVariants}
     initial="hidden"
     whileInView="show"
-    className="p-8 relative"
+    className="p-8 relative z-10"
   >
-    <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className="absolute mx-8 md:mx-12 pb-4 pt-4 w-[50%] inset-0 flex font-semibold text-white">
-      <Image src="/helloLogo.png" width={32} height={32} alt="Hello logo" />
+    <div className="relative w-full flex justify-between">
+      <p className="text-2xl font-semibold text-white flex items-center gap-2 font-[outfit]">
+        {" "}
+        hello.app
+        <Image
+          src="https://hello.app/assets/beta-e8ce8431.png"
+          width={48}
+          height={48}
+          alt="hello beta"
+        />
+      </p>
+      <button
+        type="button"
+        className="md:px-10 md:py-3 py-2 px-6 bg-gradient-to-b from-violet-500 to-violet-800 hover:cursor-pointer rounded-xl hover:from-violet-600 hover:to-violet-900"
+      >
+        Launch app
+      </button>
     </div>
   </motion.nav>
 );
